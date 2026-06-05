2382dcdb09dacdd901e95bc2b675aad1c9b43fac
gimii565 <gimii565@gmail.com>
2026-06-05 19:47:45 -0400

    add eventlet and render config

--- config.py
+++ config.py
@@ -4,9 +4,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = (
-        f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}"
-        f"@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
-    )
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = 'planilla2025'
+    SECRET_KEY = "planilla2025"
