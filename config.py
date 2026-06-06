a96d8c07d58aa5b76c6592f3524bb0783bf52798
gimii565 <gimii565@gmail.com>
2026-06-06 10:00:04 -0400

    update todos los archivos

--- config.py
+++ config.py
@@ -4,6 +4,9 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+    uri = os.getenv("DATABASE_URL", "")
+    if uri.startswith("postgres://"):
+        uri = uri.replace("postgres://", "postgresql://", 1)
+    SQLALCHEMY_DATABASE_URI = uri
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = "planilla2025"
